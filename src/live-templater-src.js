cca3c664b1dd02b3fd7cd59d5fbd2cd3550f0abf
Matthew Lickei <matthew@rentping.com>
2017-05-22 16:20:06 -0500

    Initial commit with some working code and example.

---
+++ src/live-templater-src.js
@@ -0,0 +1,139 @@
+(function($) {
+
+	let HtmlVariable = function (variableName, variable, value, type) {
+		this.variableName = variableName;
+		this.variable = variable;
+		this.value = value;
+		this.type = type;
+	};
+
+	function processHtmlForVars(html) {
+		const varRegx = /\${[^{,}]+}/g,
+			vars = html.match(varRegx);
+
+		let htmlVarArr = [],
+			htmlVars = {};
+
+		for(let idx = 0; idx < vars.length; idx ++) {
+			let variable = vars[idx],
+				varProps = variable.replace('${','').replace('}','').split('|'),
+				newVar = varProps[0],
+				defaultVal = varProps[1],
+				varType = varProps[2],
+				htmlObj = {};
+
+			let htmlVar = new HtmlVariable(newVar, '--' + newVar, defaultVal, varType);
+			htmlVarArr.push(htmlVar);
+			html = html.replace(variable, `var(${htmlVar.variable})`);
+			htmlObj[newVar] = htmlVar;
+			htmlVars = $.extend(true, htmlObj, htmlVars);
+		}
+
+		return {
+			newHtml: html,
+			htmlVarArr: htmlVarArr,
+			htmlVars: htmlVars
+		};
+	}
+
+	$.fn.liveTemplater = function (options) {
+		let finalOpts = $.extend(true, {
+
+		}, options);
+
+		return this.each(function() {
+			(new LiveTemplater($(this), finalOpts)).init();
+		});
+	};
+
+	let LiveTemplater = function($target, options) {
+		this.options = options;
+		this.$target = $target;
+	};
+
+	LiveTemplater.prototype.setupVariables = function() {
+		const results = processHtmlForVars(this.options.rawHtml);
+
+		this.processedHtml = results.newHtml;
+		this.htmlVarArr = results.htmlVarArr;
+		this.htmlVars = results.htmlVars;
+	};
+
+	LiveTemplater.prototype.getColorInput = function(htmlVar) {
+		return `<input type="color" name="${htmlVar.variableName}" id="${this.options.id}-${htmlVar.variableName}" value="${htmlVar.value}"`;
+	};
+
+	LiveTemplater.prototype.getVariableInputHtml = function(htmlVar) {
+		switch (htmlVar.type) {
+			case 'color':
+				return this.getColorInput(htmlVar);
+		}
+	};
+
+	LiveTemplater.prototype.getCSSVariableStyle = function(htmlVar) {
+		return `${htmlVar.variable}: ${htmlVar.value};`;
+	};
+
+	LiveTemplater.prototype.getVariablesHtml = function() {
+		let varsHtml = '',
+			stylesHtml = '';
+
+		for(let idx = 0; idx < this.htmlVarArr.length; idx ++) {
+			let htmlVar = this.htmlVarArr[idx];
+			varsHtml = varsHtml + `<div class="template-variable"><label for="${this.options.id}-${htmlVar.variableName}">${htmlVar.variableName}</label>${this.getVariableInputHtml(htmlVar)}</div>`;
+			stylesHtml = stylesHtml + this.getCSSVariableStyle(htmlVar);
+		}
+
+		return {
+			varsHtml: varsHtml,
+			stylesHtml: stylesHtml
+		}
+	};
+
+	LiveTemplater.prototype.getTemplateHtml = function() {
+		const variablesHtml = this.getVariablesHtml();
+
+		return `<div class="templater-container" id="${this.options.id}">
+					<div class="template-variables">${variablesHtml.varsHtml}</div>
+					<div class="live-template-preview-container">
+						<style>#${this.options.id} { ${variablesHtml.stylesHtml} }</style>
+						<div class="live-template-preview">${this.processedHtml}</div>
+					</div>
+				</div>`;
+	};
+
+	LiveTemplater.prototype.buildTemplateUI = function() {
+		this.$target.append(this.getTemplateHtml());
+	};
+
+	LiveTemplater.prototype.attachEvents = function () {
+		let $templaterContainer = $(`#${this.options.id}`),
+			templaterCont = $templaterContainer.get(0),
+			$variables = $templaterContainer.find('.template-variable'),
+			htmlVars = this.htmlVars;
+
+		$variables.on('change', function (evt) {
+			let $input = $(this).find('input'),
+				val = $input.val(),
+				htmlVar = htmlVars[$input.attr('name')];
+
+			templaterCont.style.setProperty(htmlVar.variable, val);
+		});
+	};
+
+	LiveTemplater.prototype.removeTemplateUI = function() {
+		this.$target.empty();
+	};
+
+	LiveTemplater.prototype.setupUI = function () {
+		this.removeTemplateUI();
+		this.buildTemplateUI();
+		this.attachEvents();
+	};
+
+	LiveTemplater.prototype.init = function() {
+		this.setupVariables();
+		this.setupUI();
+	};
+
+} (jQuery));
